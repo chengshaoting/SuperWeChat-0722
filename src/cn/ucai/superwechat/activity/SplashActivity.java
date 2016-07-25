00a507b15a662bdc82fdc9bd29e4c6b6143254ab
chengshaoting <1065290782@qq.com>
2016-07-25 10:09:01 +0800

    闪屏获取user

--- src/cn/ucai/superwechat/activity/SplashActivity.java
+++ src/cn/ucai/superwechat/activity/SplashActivity.java
@@ -12,10 +12,18 @@ import android.widget.TextView;
 
 import com.easemob.chat.EMChatManager;
 import com.easemob.chat.EMGroupManager;
+
+import java.util.List;
+import java.util.Map;
+
 import cn.ucai.superwechat.DemoHXSDKHelper;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.Utils;
+import cn.ucai.superwechat.bean.Result;
 import cn.ucai.superwechat.bean.UserAvatar;
+import cn.ucai.superwechat.data.OkHttpUtils2;
 import cn.ucai.superwechat.db.UserDao;
 import cn.ucai.superwechat.task.DownloadContactListTask;
 
@@ -62,11 +70,36 @@ public class SplashActivity extends BaseActivity {
 					UserDao dao = new UserDao(SplashActivity.this);
 					UserAvatar user = dao.getUserAvatar(userName);
 					Log.e(TAG, "user=" + user);
-					if (user != null) {
+					if(user==null){
+						final OkHttpUtils2<String> utils = new OkHttpUtils2<String>();
+						utils.setRequestUrl(I.REQUEST_FIND_USER)
+								.addParam(I.Contact.USER_NAME,userName)
+								.targetClass(String.class)
+								.execute(new OkHttpUtils2.OnCompleteListener<String>() {
+									@Override
+									public void onSuccess(String s) {
+										Log.e(TAG, "s="+s);
+										Result result = Utils.getListResultFromJson(s, UserAvatar.class);
+										Log.e(TAG, "result=" + result);
+										if (result!=null&&result.isRetMsg()) {
+											UserAvatar user = (UserAvatar) result.getRetData();
+											Log.e(TAG,"user="+user);
+											if (user != null) {
+												SuperWeChatApplication.getInstance().setUser(user);
+												SuperWeChatApplication.currentUserNick = user.getMUserNick();
+											}
 
-						SuperWeChatApplication.getInstance().setUser(user);
-						SuperWeChatApplication.currentUserNick = user.getMUserNick();
+											}
+									}
+
+									@Override
+									public void onError(String error) {
+										Log.e(TAG, "error=" + error);
+									}
+								});
+
 					}
+
 					new DownloadContactListTask(SplashActivity.this,userName).execute();
 //					闪屏
 
