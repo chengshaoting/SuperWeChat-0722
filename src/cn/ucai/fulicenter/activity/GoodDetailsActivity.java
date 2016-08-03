76ece35f0b22b49749916f6fb966b2b315147be4
chengshaoting <1065290782@qq.com>
2016-08-03 16:38:27 +0800

    获取商品详情数据并显示

--- src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -1,18 +1,29 @@
 package cn.ucai.fulicenter.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 import cn.ucai.fulicenter.D;
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.AlbumsBean;
+import cn.ucai.fulicenter.bean.GoodDetailsBean;
+import cn.ucai.fulicenter.data.OkHttpUtils2;
+import cn.ucai.fulicenter.view.DisplayUtil;
 import cn.ucai.fulicenter.view.FlowIndicator;
 import cn.ucai.fulicenter.view.SlideAutoLoopView;
 
 public class GoodDetailsActivity extends Activity {
+    String TAG = GoodDetailsActivity.class.getSimpleName();
     ImageView ivShare;
     ImageView ivCollect;
     ImageView ivCart;
@@ -26,15 +37,63 @@ public class GoodDetailsActivity extends Activity {
     WebView wvGoodBrief;
     int mGoodId;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good_details);
         initView();
         initData();
+        setListener();
+    }
+
+    private void setListener() {
+        final OkHttpUtils2<String> utils = new OkHttpUtils2<>();
+        utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
+                .addParam(I.Cart.GOODS_ID,String.valueOf(mGoodId))
+                .targetClass(String.class)
+                .execute(new OkHttpUtils2.OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String result) {
+                        Log.e(TAG,"result==="+result);
+                        Gson gson = new Gson();
+                        GoodDetailsBean goodDetailsBean = gson.fromJson(result, GoodDetailsBean.class);
+                        if(goodDetailsBean!=null){
+                            showGoodDetails(goodDetailsBean);
+                        }else {
+                            Toast.makeText(getApplicationContext(),"获取信息失败",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Toast.makeText(getApplicationContext(),"获取信息失败",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+    }
+
+    private void showGoodDetails(GoodDetailsBean goodDetailsBean) {
+        tvGoodEnglishName.setText(goodDetailsBean.getGoodsEnglishName());
+        tvGoodName.setText(goodDetailsBean.getGoodsName());
+        tvGoodPriceShop.setText("商品价格"+goodDetailsBean.getCurrencyPrice());
+        tvGoodPriceCurrent.setText("商品当前价格"+goodDetailsBean.getCurrencyPrice());
+        mSlideAutoLoopView.startPlayLoop(mFlowIndicator,getAlbumImageUrl(goodDetailsBean),
+                goodDetailsBean.getProperties()[0].getAlbums().length);
+        wvGoodBrief.loadDataWithBaseURL(null,goodDetailsBean.getGoodsBrief(),D.TEXT_HTML,D.UTF_8,null);
+    }
+
+    private String[] getAlbumImageUrl(GoodDetailsBean goodDetailsBean) {
+        String[] albumImageUrl = new String[]{};
+        if(goodDetailsBean.getPromotePrice()!=null&&goodDetailsBean.getProperties().length>0){
+            AlbumsBean[] albums = goodDetailsBean.getProperties()[0].getAlbums();
+            albumImageUrl=new String[albums.length];
+            for (int i=0;i<albumImageUrl.length;i++){
+                albumImageUrl[i]=albums[i].getImgUrl();
+            }
+        }
+        return albumImageUrl;
+
     }
 
     private void initData() {
@@ -56,6 +115,7 @@ public class GoodDetailsActivity extends Activity {
         WebSettings settings = wvGoodBrief.getSettings();
         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         settings.setBuiltInZoomControls(true);
+        DisplayUtil.initBack(this);
 
     }
 }
